0a49621a91287ef1bc540bcd698f0e4eae3f7f5c
SOHYUK IM <sohyukim@SOHYUK-MacBook-Air.local>
2021-08-29 18:11:09 +0900

    modify dataset & update csv

--- dataset.py
+++ dataset.py
@@ -1,247 +1,70 @@
-import os
+# System Libs.
 import random
-from collections import defaultdict
-from enum import Enum
-from typing import Tuple, List
 
-import numpy as np
+# Other Libs
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
 from PIL import Image
-from torch.utils.data import Dataset, Subset, random_split
-from torchvision import transforms as T
-from tqdm import tqdm
-
-from transform import *
-
-class MaskLabels(Enum):
-    MASK = 0
-    INCORRECT = 1
-    NORMAL = 2
 
 
-class GenderLabels(int, Enum):
-    MALE = 0
-    FEMALE = 1
+class MaskClassifierDataset(Dataset):
 
-    @classmethod
-    def from_str(cls, value: str) -> int:
-        value = value.lower()
-        if value == "male":
-            return cls.MALE
-        elif value == "female":
-            return cls.FEMALE
-        else:
-            raise ValueError(f"Gender value should be either 'male' or 'female', {value}")
-
-
-class AgeLabels(int, Enum):
-    YOUNG = 0
-    MID = 1
-    OLD = 2
-
-    @classmethod
-    def from_number(cls, value: str) -> int:
-        try:
-            value = int(value)
-        except Exception:
-            raise ValueError(f"Age value should be numeric, {value}")
-
-        if value < 30:
-            return cls.YOUNG
-        elif value < 60:
-            return cls.MID
-        else:
-            return cls.OLD
-
-
-class MaskBaseDataset(Dataset):
-    num_classes = 3 * 2 * 3
-
-    _file_names = {
-        "mask1": MaskLabels.MASK,
-        "mask2": MaskLabels.MASK,
-        "mask3": MaskLabels.MASK,
-        "mask4": MaskLabels.MASK,
-        "mask5": MaskLabels.MASK,
-        "incorrect_mask": MaskLabels.INCORRECT,
-        "normal": MaskLabels.NORMAL
-    }
-
-    image_paths = []
-    mask_labels = []
-    gender_labels = []
-    age_labels = []
-
-    def __init__(self, data_dir, mean=None, std=None):
-        self.data_dir = data_dir
-        self.mean = mean
-        self.std = std
-
-        self.transform = None
-        self.setup()
-        self.calc_statistics()
-
-    def setup(self):
-        profiles = os.listdir(self.data_dir)
-        for profile in profiles:
-            if profile.startswith("."):
-                continue
-
-            img_folder = os.path.join(self.data_dir, profile)
-            for file_name in os.listdir(img_folder):
-                _file_name, ext = os.path.splitext(file_name)
-                if _file_name not in self._file_names:  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
-                    continue
-
-                img_path = os.path.join(self.data_dir, profile, file_name)  # (resized_data, 000004_male_Asian_54, mask1.jpg)
-                mask_label = self._file_names[_file_name]
-
-                id, gender, race, age = profile.split("_")
-                gender_label = GenderLabels.from_str(gender)
-                age_label = AgeLabels.from_number(age)
-
-                self.image_paths.append(img_path)
-                self.mask_labels.append(mask_label)
-                self.gender_labels.append(gender_label)
-                self.age_labels.append(age_label)
-
-    def calc_statistics(self):
-        has_statistics = self.mean is not None and self.std is not None
-        if not has_statistics:
-            print("Calculating statistics... This might take a while")
-            sums = []
-            squared = []
-            for image_path in tqdm(self.image_paths):
-                image = np.array(Image.open(image_path)).astype(np.int32)
-                sums.append(image.mean(axis=(0, 1)))
-                squared.append((image ** 2).mean(axis=(0, 1)))
-
-            self.mean = np.mean(sums, axis=0) / 255
-            self.std = (np.mean(squared, axis=0) - self.mean ** 2) ** 0.5 / 255
-
-    def set_transform(self, transform):
+    def __init__(self, img_paths, labels, transform=None):
+        self.img_paths = img_paths
+        self.labels = labels
         self.transform = transform
 
-    def __getitem__(self, index):
-        assert self.transform is not None, ".set_tranform 메소드를 이용하여 transform 을 주입해주세요"
-
-        image = self.read_image(index)
-        mask_label = self.get_mask_label(index)
-        gender_label = self.get_gender_label(index)
-        age_label = self.get_age_label(index)
-        multi_class_label = self.encode_multi_class(mask_label, gender_label, age_label)
-
-        image_transform = self.transform(image)
-        return image_transform, multi_class_label
-
     def __len__(self):
-        return len(self.image_paths)
-
-    def get_mask_label(self, index):
-        return self.mask_labels[index].value
-
-    def get_gender_label(self, index):
-        return self.gender_labels[index].value
-
-    def get_age_label(self, index):
-        return self.age_labels[index].value
-
-    def read_image(self, index):
-        image_path = self.image_paths[index]
-        return Image.open(image_path)
-
-    @staticmethod
-    def encode_multi_class(mask_label, gender_label, age_label) -> int:
-        return mask_label * 6 + gender_label * 3 + age_label
-
-    @staticmethod
-    def decode_multi_class(multi_class_label):
-        mask_label = (multi_class_label // 6) % 3
-        gender_label = (multi_class_label // 3) % 2
-        age_label = multi_class_label % 3
-        return (mask_label, gender_label, age_label)
-
-    @staticmethod
-    def denormalize_image(image, mean, std):
-        _img = image.copy()
-        _img *= std
-        _img += mean
-        _img *= 255.0
-        _img = np.clip(_img, 0, 255).astype(np.uint8)
-        return _img
-
-    def split_dataset(self, val_size) -> Tuple[Subset, Subset]:
-        n_val = int(len(self) * val_size)
-        n_train = len(self) - n_val
-        train_set, val_set = random_split(self, [n_train, n_val])
-        return (train_set, val_set)
-
-
-class MaskSplitByProfileDataset(MaskBaseDataset):
-    def __init__(self, data_dir, mean=None, std=None):
-        super().__init__(data_dir, mean, std)
-        self.indices = defaultdict(list)
-
-    @staticmethod
-    def _split_profile(profiles, val_ratio):
-        length = len(profiles)
-        n_val = int(length * val_ratio)
-
-        val_indices = set(random.choices(range(length), k=n_val))
-        train_indices = set(range(length)) - val_indices
-        return {
-            "train": train_indices,
-            "val": val_indices
-        }
-
-    def setup(self):
-        profiles = os.listdir(self.data_dir)
-        profiles = [profile for profile in profiles if not profile.startswith(".")]
-        split_profiles = self._split_profile(profiles, self.val_ratio)
-
-        cnt = 0
-        for phase, indices in split_profiles.items():
-            for _idx in indices:
-                profile = profiles[_idx]
-                img_folder = os.path.join(self.data_dir, profile)
-                for file_name in os.listdir(img_folder):
-                    _file_name, ext = os.path.splitext(file_name)
-                    if _file_name not in self._file_names:  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
-                        continue
-
-                    img_path = os.path.join(self.data_dir, profile, file_name)  # (resized_data, 000004_male_Asian_54, mask1.jpg)
-                    mask_label = self._file_names[_file_name]
-
-                    id, gender, race, age = profile.split("_")
-                    gender_label = GenderLabels.from_str(gender)
-                    age_label = AgeLabels.from_number(age)
-
-                    self.image_paths.append(img_path)
-                    self.mask_labels.append(mask_label)
-                    self.gender_labels.append(gender_label)
-                    self.age_labels.append(age_label)
-
-                    self.indices[phase].append(cnt)
-                    cnt += 1
-
-    def split_dataset(self) -> List[Subset]:
-        return [Subset(self, indices) for phase, indices in self.indices.items()]
-
-
-class TestDataset(Dataset):
-    def __init__(self, img_paths, resize, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)):
-        self.img_paths = img_paths
-        self.transform = BaseTransform(
-            resize=resize,
-            mean=mean,
-            std=std
-        )
+        return len(list(img_paths))
 
     def __getitem__(self, index):
         image = Image.open(self.img_paths[index])
+        label = torch.tensor(self.labels[index])
 
         if self.transform:
             image = self.transform(image)
-        return image
 
-    def __len__(self):
-        return len(self.img_paths)
+        return (image, label)
+
+
+def train_test_split_df(data_df, crit_col='path', test_size=0.2, shuffle=True, random_state=None):
+    if random_state:
+        random.seed(random_state)
+
+    _idxs = set(data_df[crit_col].unique())
+    _size = len(_idxs)
+    _size_test = int(_size * test_size)
+
+    # Split DataFrame
+    test_idxs = set(random.sample(_idxs, _size_test))
+    test_df = data_df.loc[data_df[crit_col].isin(test_idxs)]
+
+    train_idxs = _idxs - test_idxs
+    train_df = data_df.loc[data_df[crit_col].isin(train_idxs)]
+
+    return (train_df, test_df)
+
+
+def dist_analysis(df_dict, col_list=['Mask', 'Age', 'Gender']):
+    dist_list = []
+    for name, df in df_dict.items():
+        dist_df_list = []
+        for col in col_list:
+            # Dist. info
+            dist_count = pd.DataFrame(df[col].value_counts())
+            dist_count.columns = ['Count']
+            dist_ratio = pd.DataFrame(df[col].value_counts(True))
+            dist_ratio.columns = ['Ratio']
+
+            # Construct & append dataframe
+            _dist_df = pd.concat([dist_count, dist_ratio], axis=1)
+            _dist_df.index = pd.MultiIndex.from_product(
+                [[col], _dist_df.index])
+            dist_df_list.append(_dist_df)
+        dist_df = pd.concat(dist_df_list, axis=0)
+        dist_df.columns = pd.MultiIndex.from_product([[name], dist_df.columns])
+        dist_list.append(dist_df)
+    dist_info = pd.concat(dist_list, axis=1)
+
+    return dist_info
