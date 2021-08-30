ff917a862e9a2cc68a8065c640d9a58c9ae4c75e
kwanghyoun <kwanghyoun.yi@gmail.com>
2021-08-30 07:34:24 +0000

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -73,12 +73,6 @@ class TrainInfo():
 
 
 class MaskBaseDataset(Dataset):
-    """
-    훈련 데이터 생성 클래스
-
-    Args:
-
-    """
 
     def __init__(self, data_dir, mean=None, std=None, train_dir=None):
         self.data_dir = data_dir
@@ -169,59 +163,6 @@ class MaskBaseDataset(Dataset):
         n_train = len(self) - n_val
         train_set, val_set = random_split(self, [n_train, n_val])
         return (train_set, val_set)
-
-
-class MaskSplitByProfileDataset(MaskBaseDataset):
-    def __init__(self, data_dir, mean=None, std=None):
-        super().__init__(data_dir, mean, std)
-        self.indices = defaultdict(list)
-
-    @staticmethod
-    def _split_profile(profiles, val_ratio):
-        length = len(profiles)
-        n_val = int(length * val_ratio)
-
-        val_indices = set(random.choices(range(length), k=n_val))
-        train_indices = set(range(length)) - val_indices
-        return {
-            "train": train_indices,
-            "val": val_indices
-        }
-
-    def setup(self):
-        profiles = os.listdir(self.data_dir)
-        profiles = [
-            profile for profile in profiles if not profile.startswith(".")]
-        split_profiles = self._split_profile(profiles, self.val_ratio)
-
-        cnt = 0
-        for phase, indices in split_profiles.items():
-            for _idx in indices:
-                profile = profiles[_idx]
-                img_folder = os.path.join(self.data_dir, profile)
-                for file_name in os.listdir(img_folder):
-                    _file_name, ext = os.path.splitext(file_name)
-                    if _file_name not in self._file_names:  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
-                        continue
-
-                    # (resized_data, 000004_male_Asian_54, mask1.jpg)
-                    img_path = os.path.join(self.data_dir, profile, file_name)
-                    mask_label = self._file_names[_file_name]
-
-                    id, gender, race, age = profile.split("_")
-                    gender_label = GenderLabels.from_str(gender)
-                    age_label = AgeLabels.from_number(age)
-
-                    self.image_paths.append(img_path)
-                    self.mask_labels.append(mask_label)
-                    self.gender_labels.append(gender_label)
-                    self.age_labels.append(age_label)
-
-                    self.indices[phase].append(cnt)
-                    cnt += 1
-
-    def split_dataset(self) -> List[Subset]:
-        return [Subset(self, indices) for phase, indices in self.indices.items()]
 
 
 class TestDataset(Dataset):
