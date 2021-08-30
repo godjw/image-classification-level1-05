e9ec4ef7f401e69bea5c22e701c220901826e710
Minsoo Kim <min-soo.kim@outlook.kr>
2021-08-30 12:52:43 +0000

    confusion matrix added

--- train.py
+++ train.py
@@ -190,7 +190,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--seed', type=int, default=42, help='random seed (default: 42)')
     parser.add_argument('--epochs', type=int, default=5, help='number of epochs to train (default: 5)')
-    parser.add_argument('--dataset', type=str, default='MaskClassifierDataset', help='dataset transform type (default: MaskBaseDataset)')
+    parser.add_argument('--dataset', type=str, default='MaskBaseDataset', help='dataset transform type (default: MaskBaseDataset)')
     parser.add_argument('--transform', type=str, default='BaseTransform', help='data transform type (default: BaseTransform)')
     parser.add_argument("--resize", nargs="+", type=list, default=(128, 96), help='resize size for image when training')
     parser.add_argument('--batch_size', type=int, default=64, help='input batch size for training (default: 64)')
