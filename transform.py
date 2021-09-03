b37c44b7454eac7b9d3b80e4cb5657a51369054f
Minsoo Kim <min-soo.kim@outlook.kr>
2021-09-03 05:55:03 +0000

    Gaussian noise deleted

--- transform.py
+++ transform.py
@@ -1,18 +1,4 @@
-import torch
-
 from torchvision import transforms as T
-
-
-class AddGaussianNoise:
-    def __init__(self, mean=0.0, std=1.0):
-        self.std = std
-        self.mean = mean
-
-    def __call__(self, tensor):
-        return tensor + torch.randn(tensor.size()) * self.std + self.mean
-
-    def __repr__(self):
-        return self.__class__.__name__ + f"mean: {self.mean:.2f}, std: {self.std:.2f}"
 
 
 class BaseTransform:
