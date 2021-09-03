c5482f609b74733a15223c22f2b74a797f77b96b
Minsoo Kim <min-soo.kim@outlook.kr>
2021-09-03 06:10:21 +0000

    Docstrings added

--- transform.py
+++ transform.py
@@ -2,11 +2,19 @@ from torchvision import transforms as T
 
 
 class BaseTransform:
+    """
+    Base tranform class that apllies to validation dataset.
+    It contains [Resize, ToTensor, Normalize] by default.
+
+    Args:
+        resize (sequence): Size that an image is resized to
+        mean (sequence): Sequence of means for each channel
+        std (sequence): Sequence of standard deviations for each channel
+    """
+
     def __init__(self, resize, mean, std):
         self.transforms = [
-            T.CenterCrop((360, 300)),
             T.Resize(resize, T.InterpolationMode.BICUBIC),
-            
             T.ToTensor(),
             T.Normalize(mean=mean, std=std),
         ]
@@ -16,6 +24,16 @@ class BaseTransform:
 
 
 class CustomTransform(BaseTransform):
+    """
+    Custom tranform class that apllies to train dataset.
+    It contains [BaseTransform, RandomHorizontalFlip(p=0.5)] by default.
+
+    Args:
+        resize (sequence): Size that an image is resized to
+        mean (sequence): Sequence of means for each channel
+        std (sequence): Sequence of standard deviations for each channel
+    """
+
     def __init__(self, resize, mean, std):
         super().__init__(resize=resize, mean=mean, std=std)
         self.transforms = [*self.transforms, T.RandomHorizontalFlip(p=0.5)]
